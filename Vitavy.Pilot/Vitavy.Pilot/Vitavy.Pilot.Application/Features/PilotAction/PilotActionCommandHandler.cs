dbf35749e7f25346cf7b35b48e6b377ad004b0c3
Tonio <dev@dics.consulting>
2025-04-28 01:11:08 +0200

    Corection du calcul métier de température

--- Vitavy.Pilot/Vitavy.Pilot/Vitavy.Pilot.Application/Features/PilotAction/PilotActionCommandHandler.cs
+++ Vitavy.Pilot/Vitavy.Pilot/Vitavy.Pilot.Application/Features/PilotAction/PilotActionCommandHandler.cs
@@ -9,7 +9,7 @@ public class PilotActionCommandHandler : IRequestHandler<PilotActionCommand, Res
     public async Task<Result<LauchPilotActionResponse>> Handle(PilotActionCommand request, CancellationToken cancellationToken)
     {
         var random = new Random();
-        var temperature = random.Next(400)/10 as decimal? ?? 0;
+        var temperature = (decimal)random.Next(400)/10;
         return await Task.FromResult(
             Result.Ok(
                 new LauchPilotActionResponse(
