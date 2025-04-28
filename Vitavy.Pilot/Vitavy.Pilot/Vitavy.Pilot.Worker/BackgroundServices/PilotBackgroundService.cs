31c2778b1dc272388d0a8b31019a5cf2d2a18ef9
Anthony Dicanot <anthony.dicanot@groupecarso.com>
2025-04-28 11:01:43 +0200

    Moved Subscription into Application layer

--- Vitavy.Pilot/Vitavy.Pilot/Vitavy.Pilot.Worker/BackgroundServices/PilotBackgroundService.cs
+++ Vitavy.Pilot/Vitavy.Pilot/Vitavy.Pilot.Worker/BackgroundServices/PilotBackgroundService.cs
@@ -1,27 +1,14 @@
-using AutoMapper;
 using MediatR;
-using Vitavy.Infrastructure.Abtraction.Contracts;
-using Vitavy.Pilot.Application.Features.PilotAction;
-using Vitavy.Pilot.Application.Models;
-using Vitavy.Pilot.Worker.Constants;
+using Vitavy.Pilot.Application.Features.BackgroundServiceAction;
 
 namespace Vitavy.Pilot.Worker.BackgroundServices;
 
-public class PilotBackgroundService(ILogger<PilotBackgroundService> logger, IMediator mediator, IEventBusConsumer eventBusConsumer, IMapper mapper) : BackgroundService
+public class PilotBackgroundService(ILogger<PilotBackgroundService> logger, IMediator mediator) : BackgroundService
 {
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
         logger.LogInformation("Vitavy pilot Background Service is starting.");
-        await eventBusConsumer.Subscribe(
-            async (LauchPilotActionMessage message, CancellationToken cancellationToken) =>
-            {
-                var command = mapper.Map<PilotActionCommand>(message);
-                var result = await mediator.Send(command, cancellationToken);
-                logger.LogInformation($"Vitavy pilot Background Service is working.");
-                return result;
-            },
-            RoutingConstants.ConsumerId,
-            stoppingToken);
+        await mediator.Send(new BackgroundServiceActionCommand(), stoppingToken);
         logger.LogInformation("Vitavy pilot Background Service has started.");
     }
 }
